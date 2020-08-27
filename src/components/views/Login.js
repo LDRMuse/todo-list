b70e6eb684cc61f35e72599a1c65b8e5cab41dff
Melissa <melissakheying@gmail.com>
2020-08-27 12:23:11 -0500

    refactor

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,48 +1,27 @@
 import React from "react"
 import * as Yup from 'yup'
 
-import { useFormik } from "formik"
+import { Formik } from "formik"
 
 export const Login = () => {
-  const validate = (values) => {
-    const errors = {}
-
-  //   if (!values.name) {
-  //     errors.name = "Name is required!"
-  //   }
-  //   if (!values.email) {
-  //     errors.email = "Email is required!"
-  //   } else if (
-  //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-  //   ) {
-  //     errors.email = "Doesn't look like email"
-  //   }
-  //   if (!values.password) {
-  //     errors.password = "Password is required!"
-  //   }
-
-  //   return errors
-  }
-
-  const formik = useFormik({
-    // This is like 'setState'
-    initialValues: {
+  return (
+    <Formik
+    initialValues={{
       email: "",
       name: "",
       password: "",
-    },
-    validationSchema: Yup.object({
+    }}
+    valiationSchema={Yup.object({
       name: Yup.string().required('Name is required'),
       email: Yup.string().email('invalid email').required('Email is required'),
       password: Yup.string().min(6).required('Password is required'),
-    }),
-    onSubmit: (values) => {
-      // This is like utility fxn. that gathers all values
+    })}
+    onSubmit={(values) => {
       console.log("submission", values)
-    },
-  })
-
-  return (
+      {/* setSubmitting(false) */}
+    }}
+    >
+    {(formik) => (
     <form onSubmit={formik.handleSubmit}>
       <label htmlFor="name">Name</label>
       <input
@@ -76,5 +55,9 @@ export const Login = () => {
 
       <button type="submit">Submit</button>
     </form>
+  )}
+    </Formik>
   )
+
+
 }
