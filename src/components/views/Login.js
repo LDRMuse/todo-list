48fe6aa912a5730df90d2f00297d21641fc872f1
Melissa <melissakheying@gmail.com>
2020-08-27 11:53:21 -0500

    :construction: validation touched.inputs

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,52 +1,83 @@
-import React from 'react'
-import { useFormik } from 'formik'
+import React from "react"
+
+import { useFormik } from "formik"
 
 export const Login = () => {
-
   const validate = (values) => {
-    const errors = {};
+    const errors = {}
+
     if (!values.name) {
-      errors.name = "Name is required"
-    } if (!values.email) {
-      errors.email = "Email is required"
-    } if (!values.password) {
-      errors.password = "Password is required"
+      errors.name = "Name is required!"
     }
+    if (!values.email) {
+      errors.email = "Email is required!"
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Doesn't look like email"
+    }
+    if (!values.pass) {
+      errors.pass = "Password is required!"
+    }
+
     return errors
   }
 
-
-
   const formik = useFormik({
+    // This is like 'setState'
     initialValues: {
-      // just like setState
-      email: '',
-      name: '',
-      password: '',
+      email: "",
+      name: "",
+      pass: "",
     },
     validate,
     onSubmit: (values) => {
-      // this is like utility function that gathers all VALUES
-      console.log('submission', values)
-    }
+      // This is like utility fxn. that gathers all values
+      console.log("submission", values)
+    },
   })
-
-
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email}
-      />
-      {formik.errors.email ? <p>{formik.errors.email}</p> : null}
       <label htmlFor="name">Name</label>
-      <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name}
-       />
-      {formik.errors.name ? <p>{formik.errors.name}</p> : null}
-      <label htmlFor="password">Password</label>
-      <input type='password' id='password' name='password' onChange={formik.handleChange} value={formik.values.password}
-       />
-      {formik.errors.password ? <p>{formik.errors.password}</p> : null}
+      <input
+        type="text"
+        id="name"
+        name="name"
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      {formik.touched.name && formik.errors.name ? (
+        <p>{formik.errors.name}</p>
+      ) : null}
+
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      {formik.touched.email && formik.errors.email ? (
+        <p>{formik.errors.email}</p>
+      ) : null}
+
+      <label htmlFor="pass">Password</label>
+      <input
+        type="password"
+        id="pass"
+        name="pass"
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange}
+        value={formik.values.pass}
+      />
+      {formik.touched.password && formik.errors.pass ? (
+        <p>{formik.errors.pass}</p>
+      ) : null}
+
       <button type="submit">Submit</button>
     </form>
   )
