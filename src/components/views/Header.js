93965478e7dcaf6b32cec65033e238345a73a8b4
Melissa <melissakheying@gmail.com>
2020-08-27 13:48:54 -0500

    Link header to home page

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -1,19 +1,16 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <section className="hero has-text-centered">
-  <div className="hero-body">
-    <div className="container">
-      <h1 className="title">
-        Todo List!
-      </h1>
-      <h2 className="subtitle">
-        A list of things to do
-      </h2>
-    </div>
-  </div>
-</section>
+        <div className="hero-body">
+          <div className="container">
+          <h1 className="title"><Link to="/">Todo List!</Link></h1>
+            <h2 className="subtitle">A list of things to do</h2>
+          </div>
+        </div>
+
+    </section>
   )
 }
