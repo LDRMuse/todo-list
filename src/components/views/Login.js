10a52cb0a398f28efdf42affc7d74fd1080b06c7
Melissa <melissakheying@gmail.com>
2020-08-27 11:35:57 -0500

    useFormik and Add basic if statement to validate

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import { useFormik } from 'formik'
 
 export const Login = () => {
 
+  const validate = (values) => {
+    const errors = {};
+    if (!values.name) {
+      errors.name = "Name is required"
+    } if (!values.email) {
+      errors.email = "Email is required"
+    } if (!values.password) {
+      errors.password = "Password is required"
+    }
+    return errors
+  }
+
+
+
+  const formik = useFormik({
+    initialValues: {
+      // just like setState
+      email: '',
+      name: '',
+      password: '',
+    },
+    validate,
+    onSubmit: (values) => {
+      // this is like utility function that gathers all VALUES
+      console.log('submission', values)
+    }
+  })
+
+
+
   return (
-    <p>Login</p>
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email}
+      />
+      {formik.errors.email ? <p>{formik.errors.email}</p> : null}
+      <label htmlFor="name">Name</label>
+      <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name}
+       />
+      {formik.errors.name ? <p>{formik.errors.name}</p> : null}
+      <label htmlFor="password">Password</label>
+      <input type='password' id='password' name='password' onChange={formik.handleChange} value={formik.values.password}
+       />
+      {formik.errors.password ? <p>{formik.errors.password}</p> : null}
+      <button type="submit">Submit</button>
+    </form>
   )
 }
